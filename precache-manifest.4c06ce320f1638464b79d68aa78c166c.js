ef0bbfe8bfb5fb03424fa36f1781d7239cbbe3ed
Girija Viswanathan <girijavmohan@gmail.com>
2020-06-02 11:55:20 -0700

    Updates

---
+++ precache-manifest.4c06ce320f1638464b79d68aa78c166c.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57fd697efde8b15d7af8f3de47e4c5a7",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "142e6668f037cfe38f63",
+    "url": "/portfolio/static/css/2.3d60f3ae.chunk.css"
+  },
+  {
+    "revision": "d9a780269787a1f83030",
+    "url": "/portfolio/static/css/main.7b83ca65.chunk.css"
+  },
+  {
+    "revision": "142e6668f037cfe38f63",
+    "url": "/portfolio/static/js/2.8de3f993.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.8de3f993.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9a780269787a1f83030",
+    "url": "/portfolio/static/js/main.8c160455.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  },
+  {
+    "revision": "906ee164c0a174facf1e5fe9fb17dd7f",
+    "url": "/portfolio/static/media/B1.906ee164.png"
+  },
+  {
+    "revision": "05511443fc0c953602e430e7b5f9bc06",
+    "url": "/portfolio/static/media/B2.05511443.png"
+  },
+  {
+    "revision": "41ffd4258e9d6b27b75a27df3127bbd7",
+    "url": "/portfolio/static/media/B3.41ffd425.png"
+  },
+  {
+    "revision": "f8ddd9c21cb1f40eddd14bcf2331b0dd",
+    "url": "/portfolio/static/media/B4.f8ddd9c2.png"
+  },
+  {
+    "revision": "dc89aca775a1b6d57732815b6859a13f",
+    "url": "/portfolio/static/media/B5.dc89aca7.png"
+  },
+  {
+    "revision": "3cf23a59fdc87ffc0433bac75ee1f863",
+    "url": "/portfolio/static/media/GV-closeup.3cf23a59.jpeg"
+  },
+  {
+    "revision": "89b16c927f54499c40f8fd98432b092e",
+    "url": "/portfolio/static/media/GV1.89b16c92.jpeg"
+  },
+  {
+    "revision": "3c318df8a68e866b7471468a0c9ca2f5",
+    "url": "/portfolio/static/media/d2.3c318df8.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "2ea6cdc4d5fa4049b8c0f0bac609a355",
+    "url": "/portfolio/static/media/kothaiFoods.2ea6cdc4.jpeg"
+  },
+  {
+    "revision": "5937c08bb71ac7f74561e6d683c7e5ee",
+    "url": "/portfolio/static/media/wipro-logo.5937c08b.jpg"
+  }
+]);
