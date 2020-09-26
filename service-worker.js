f607fff00b84f8178821287b8d5cf528553ab59b
Girija Viswanathan <girijavmohan@gmail.com>
2020-09-26 11:00:49 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.9ea96e5db28b0fb59cbd133f4bddfa76.js"
+  "/portfolio/precache-manifest.6d441d07ca4369871fe96c6efcee0fc6.js"
 );
 
 self.addEventListener('message', (event) => {
